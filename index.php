c9c8e0f4aaa743f89a143f8ad6021c0d9847df85
Kodintsev Andrey <kodintsevandrey@gmail.com>
2017-05-02 14:37:34 +0300

    fonts fix

--- index.php
+++ index.php
@@ -56,6 +56,7 @@ if(isset($_SESSION['utm_content'])) {
     <title>CM Project</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic-ext" rel="stylesheet">
     <link rel="stylesheet" href="./css/style.min.css" >
     <meta name="Description" content="">
     <meta name="Keywords" content="">
